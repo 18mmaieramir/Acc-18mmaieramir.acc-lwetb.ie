2d453e10fcc3b7ec3d76478b1da0b6162c0252bd
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2021-10-08 15:03:04 +0100

    Add files via upload

---
+++ worksheet.py
@@ -0,0 +1,43 @@
+#1
+name=input("Please write your first and second name")
+print("Welcome" + name)
+
+age=float(input("What is your age?: "))
+print("you are" , age)
+
+height=float(input("how tall are you?: "))
+print("you are" + name , age, "years old and" , height, "meters tall")
+
+
+firstnumber=float(input("Please write a number"))
+secondnumber=float(input("please write another number"))
+print(firstnumber + secondnumber)
+
+
+thirdnumber=float(input("Please write a number"))
+fourthnumber=float(input("please write another number"))
+print(thirdnumber - fourthnumber)
+
+fifthnumber=float(input("Please write a number"))
+sixthnumber=float(input("please write another number"))
+print(fifthnumber * sixthnumber)
+
+seventhnumber=float(input("Please write a number"))
+eighthnumber=float(input("please write another number"))
+print(seventhnumber / eighthnumber)
+
+ninethnumber=float(input("Please write an odd number"))
+tenthnumber=float(input("please write an even number"))
+print(ninethnumber // tenthnumber)
+
+
+eleventhnumber=float(input("Please write an odd number"))
+twelvethnumber=float(input("please write an even number"))
+print(eleventhnumber % twelvethnumber)
+
+average1=float(input("please write a number"))
+average2=float(input("please write another number"))
+average3=float(input("please write another number"))
+print(average1 + average2 + average3 / 3)
+
+

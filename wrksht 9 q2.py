8ef4e82b0d359f9591815498f2fbb27235be34de
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2021-11-24 15:11:07 +0000

    Add files via upload

---
+++ wrksht 9 q2.py
@@ -0,0 +1,91 @@
+list= [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+#1
+x= 1
+
+list_1 = x
+
+print(list_1)
+
+#2
+i = 3
+print(list[i])
+
+#3
+print(list[-i])
+
+#4
+j = 9
+print(list[i:j])
+
+#5
+
+print(list[i:])
+
+#6
+
+del list[i]
+
+#7
+
+print(list.append(x))
+
+#8
+L = [24]
+print(list.extend(L))
+print(list)
+
+#9
+
+list.insert(i,x)
+print(list)
+
+#10
+
+list.remove(x)
+print(list)
+
+#11
+
+list.pop(i)
+print(i)
+
+#12
+
+list.clear()
+print(list)
+
+#13
+list = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+print(list.index(x))
+
+#14
+print(list.count(x))
+
+#15
+
+list.reverse()
+print(list)
+
+#16
+
+print(list.copy())
+
+#17
+
+list.sort()
+print(list)
+
+#18
+
+sorted(L)
+print(list)
+
+
+
+
+
+
+
+
+

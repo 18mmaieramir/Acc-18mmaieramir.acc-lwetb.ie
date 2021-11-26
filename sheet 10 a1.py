8e9aee9816f21fdfab463f35b14713d9b9e1458a
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2021-11-26 15:13:35 +0000

    Add files via upload

---
+++ sheet 10 a1.py
@@ -0,0 +1,10 @@
+file = open("100Random.txt", "r")
+print(file.read())
+
+x=0
+
+
+for i range(
+
+
+
---
+++ sheet10 q1v2.py
@@ -0,0 +1,10 @@
+file1 = open("10Random.txt" , "r")
+print(file1.read())
+
+x= 24
+y= 97
+
+range1 = (y-x)
+print("range =" ,range1)
+
+

8104b418bba538a7f5c607a1a3605716379d54ef
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-01-20 12:36:02 +0000

    Add files via upload

--- bs q2.py
+++ bs q2.py
@@ -1,23 +1,27 @@
 
-L= [5, 7, 3, 6, 2, 4, 1]
-print("INPUT (initial list): ", L)
-exchange = True
-n = len(L)
-i = 0
-def bbs():
+def bbs(L):
+    print("INPUT (initial list): ", L)
+    exchange = True
+    n = len(L)
+    i = 0
+
     while (i< n) and  exchange:
-        print("BEFORE PASS %d: %s " %(i+1, L))
-    
+            print("BEFORE PASS %d: %s " %(i+1, L))
+        
     exchange = False
     for j in range(n-i-1):
-        print("%s " %L, end="-> ")
-        if L[j] > L[j+1]:
-            L[j], L[j+1] = L[j+1], L[j]
-            exchange = True
-            print("%s " %L)
-print("AFTER PASS %d: %s " %(i+1, L))
+            print("%s " %L, end="-> ")
+            if L[j] > L[j+1]:
+                L[j], L[j+1] = L[j+1], L[j]
+                exchange = True
+                print("%s " %L)
+    print("AFTER PASS %d: %s " %(i+1, L))
 
-i= i+1 # increment the loop counter
-print("OUTPUT (sorted list): ", L)
+    i= i+1 # increment the loop counter
+    print("OUTPUT (sorted list): ", L)
 
 
+
+Q= [5,4 , 3, 6, 2, 4, 1]
+
+bbs(Q)

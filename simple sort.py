97c5b8d93c881c796e2c4137cdd03d4d9f9c57e0
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-01-26 14:39:44 +0000

    Add files via upload

---
+++ simple sort.py
@@ -0,0 +1,27 @@
+# Simple (Selection) Sort
+
+aList = [9, 6, 10, 4, 8, 5, 7]
+
+marker = 0
+
+while marker < len(aList):
+    
+    index_of_min = marker
+    for j in range(marker+1, len(aList)):
+         if aList[index_of_min] > aList[j]:
+             index_of_min = j
+             
+             
+             
+    temp = aList[marker] 
+    aList[marker] = aList[index_of_min] 
+    aList[index_of_min] = temp 
+
+    marker = marker+1
+    
+    print(aList)
+    
+             
+
+
+

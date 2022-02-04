c35768075c5514aa1446deded8e9fc359fd8b60b
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-02-04 15:15:34 +0000

    Add files via upload

---
+++ coding test.py
@@ -0,0 +1,89 @@
+#1
+L =[23,1,0,-12,48, 15, 9, 20, -11, 42,7, -5, 13]
+
+g = sum(L[1::2])
+print(g)
+
+#2
+
+m = [l * 2 for l in L]
+print(m)
+
+
+
+
+
+#3
+for i in range(0,10001):
+    print(i)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#4
+o="codeHappy"
+
+for i in o:
+    print(i)
+    
+    
+#5
+length = float(input("enter length"))
+width = float(input("enter width"))
+print(length*width)
+
+#6
+
+
+p = str(input("enter text"))
+
+while p != "z":
+    p=str(input("enter text"))
+    
+    
+
+    
+    
+    
+    
+#7
+
+f = open("CS04022022.txt","w")
+
+f.write("Leunig –How To Get There\n")
+f.write("Go to the end of the path until you get to the gate.\n")
+f.write("Go through the gate and head straight out towards the horizon.\n")
+f.write("Keep going towards the horizon.\n")
+f.write("Sit down and have a rest every now and again,\n")
+f.write("But keep on going, just keep on with it\n")
+f.write("Keep on going as far as you can\n")
+f.write("That is how you get there.\n")
+
+f.close()
+
+
+
+        
+    
+    
+    
+    
+    
+
+

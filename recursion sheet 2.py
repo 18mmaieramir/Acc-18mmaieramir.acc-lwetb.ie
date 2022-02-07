1848b05d02911cb3c3c3a58f85fc35f4d788e487
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-02-07 09:40:54 +0000

    Add files via upload

---
+++ recursion sheet 2.py
@@ -0,0 +1,22 @@
+#1
+Q=int(input("enter a value"))
+
+def recurse(Q):
+  
+    
+    if (Q == 1):
+        return Q
+    else:
+        return Q + recurse(Q-1)
+    
+print(recurse(Q))
+
+
+#2
+
+    
+   
+
+
+
+

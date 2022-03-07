627a2f8d4045efe207f6929892fe6ade499d2c58
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-03-07 09:40:23 +0000

    Add files via upload

---
+++ pyplot test.py
@@ -0,0 +1,21 @@
+from matplotlib import pyplot 
+
+
+x=[1,2,3,4,5]
+fx=[3,5,9,8,7]
+
+pyplot.scatter(x,fx)
+pyplot.show()
+
+pyplot.title("test")
+pyplot.xlabel("x-axis")
+pyplot.yaxis("y-axis")
+
+
+
+
+
+
+
+
+pyplot.show()

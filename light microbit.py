609652febb929e510d460d874d9e8902d812c51b
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-03-14 09:37:46 +0000

    Add files via upload

---
+++ light microbit.py
@@ -0,0 +1,27 @@
+f = open("light.txt" ,"a+")
+
+
+def light():
+
+    import serial
+
+
+
+    serialPort = serial.Serial(port = "COM3", baudrate=115200,
+                           bytesize=8, timeout=2, stopbits=serial.STOPBITS_ONE)
+
+
+    serialString=""
+
+
+
+
+    while(1):
+        if(serialPort.in_waiting > 0):
+            serialString= serialPort.readline()
+            print(serialString.decode("Ascii"))
+            
+            
+light()
+
+f.write(light)

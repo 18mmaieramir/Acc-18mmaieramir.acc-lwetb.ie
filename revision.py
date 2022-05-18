cdd4a22426dc9324f80f7f30528f2acc80c1f082
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-05-18 15:13:13 +0100

    Add files via upload

--- revision.py
+++ revision.py
@@ -1,14 +1,34 @@
 f=open("code.txt" ,"r")
-
+f1=open("code2.txt" ,"a+")
 
 x=f.readline()
-
+#1
 
 y=x.split()
 
+m="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
 for i in y:
-    if i == int:
-        print(i)
+    for j in i:
+        if j in m:
+            f1.write(j)
+            
+            
+
         
-    else:
-        
+
+
+f1.write(str(len(y)))
+
+space=len(y) -1
+f1.write(str(space))
+
+g="1234567890"
+l=[]
+for r in y:
+    for a in r:
+        if a in g:
+            l.append(a)
+            f1.write(str(len(l)))
+             
+f1.close()

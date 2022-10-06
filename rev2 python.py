ae57d2ddcc283f9a7c85184837a73b106abd5919
18mmaieramir <92168014+18mmaieramir@users.noreply.github.com>
2022-10-06 13:15:25 +0100

    Add files via upload

---
+++ rev2 python.py
@@ -0,0 +1,63 @@
+#1
+x1= float(input("Enter first value"))
+x2= float(input("Enter secondary value"))
+
+s= x1+x2
+s = round(s,2)
+print("sum =",s)
+
+d= x1-x2
+d = round(d,2)
+print("difference =",d)
+
+m= x1*x2
+m = round(m,2)
+print("product =",m)
+
+#2
+d5= float(input("Enter 5 digit number"))
+
+ms= d5 / 60
+print(round(ms))
+
+#3
+v1= str(input("Enter first value"))
+v2= str(input("Enter first value"))
+v3= str(input("Enter first value"))
+v4= str(input("Enter first value"))
+v5= str(input("Enter first value"))
+
+print(ord(v1))
+print(ord(v2))
+print(ord(v3))
+print(ord(v4))
+print(ord(v5))
+
+
+
+#4
+r = int(input("enter value"))
+counter = 0
+total = 0
+while r > 0:
+    total = total +r
+    counter = counter+1
+    
+    r = int(input("enter value"))
+  
+
+    avg = total / counter
+
+   
+print(avg)
+
+#5
+
+v1= str(input("Enter first value"))
+v2= str(input("Enter first value"))
+v3= str(input("Enter first value"))
+v4= str(input("Enter first value"))
+v5= str(input("Enter first value"))
+
+
+
